a591b39cdbcb95b783a1176213a9390d2103f811
addison-reyes <161366730+addison-reyes@users.noreply.github.com>
2024-04-04 10:41:05 -0400

    meal details screen added

--- lib/screens/meals_screen.dart
+++ lib/screens/meals_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -12,11 +13,8 @@ class MealsScreen extends StatelessWidget {
     Widget content = ListView.builder(
       itemCount: meals.length,
       itemBuilder: (context, index) {
-        return Text(
-          meals[index].title,
-          style: Theme.of(context).textTheme.headlineLarge!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+        return MealItem(
+          meal: meals[index],
         );
       },
     );
